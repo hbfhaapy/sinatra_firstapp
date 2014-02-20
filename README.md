aa3f4a7a36ec55e6e1e8e5aa8b673c5317c03d27
BaifanHe <huopo125@163.com>
2014-02-20 21:24:27 +0800

    init a sinatra project

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a barebones Ruby app using the Sinatra framework.
+
+cd appdir
+bundle
+foreman start
---
+++ web.rb
@@ -0,0 +1,8 @@
+require 'sinatra'
+
+get '/' do
+	"Hello, world"
+end
+get '/hello' do
+	"This is the first time to learn sinatra!"
+end
